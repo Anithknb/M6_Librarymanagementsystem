c69e57e056e44045a8f110cf64f53d94017d4dc7
Anithknb <98871308+Anithknb@users.noreply.github.com>
2022-04-06 07:10:02 +0530

    Add files via upload

---
+++ 3_Implementation/library.py
@@ -0,0 +1,30 @@
+class Library:
+    college = "ABC"
+    def __init__(self, listOfBooks):
+        self.books = listOfBooks
+        self.book_index = range(len(listOfBooks))
+        self.bookAlreadyIssued = [0] * len(listOfBooks)
+        self.issuedTo = {}
+    
+    
+
+    def issueBook(self, student, book):
+        if book in self.books and self.bookAlreadyIssued[self.books.index(book)]==0:
+            print("Issued")
+            self.issuedTo[book] = student.name
+            self.bookAlreadyIssued[self.books.index(book)] = 1
+        else:
+            if book not in self.books:
+                print("Sorry this book is not in the library")
+            else:
+                print("Sorry the book is already issued\nPlease issue some other book")
+
+    def returnBook(self, student, book):
+        if book in self.books and self.bookAlreadyIssued[self.books.index(book)]==1:
+            print("Book returned successfully")
+            self.bookAlreadyIssued[self.books.index(book)]=0
+        else:
+            if book not in self.books:
+                print("Sorry the book you are trying to return does not belong to this library")
+            else:
+                print("Book already returned")
